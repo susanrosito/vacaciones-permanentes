0b7856a8a502b24db3d03a341089189504a0be77
susanrosito <rosito.susana@gmail.com>
2015-04-26 14:10:50 -0300

    adding suggested changes

--- angular/services/tripService.js
+++ angular/services/tripService.js
@@ -6,7 +6,7 @@ app.factory('tripService', ['$http', 'authService', function ($http, authService
 
     var tripService = {};
     tripService.all = [];
-    tripService.getIndexBYId = function(id) {
+    tripService.getIndexById = function(id) {
         function arrayObjectIndexOf(myArray, searchTerm, property) {
             for(var i = 0, len = myArray.length; i < len; i++) {
                 if (myArray[i][property] === searchTerm) return i;
@@ -32,7 +32,7 @@ app.factory('tripService', ['$http', 'authService', function ($http, authService
     };
     tripService.delete = function (trip) {
         return $http.delete(remote_uri(trip._id), authService.getHeader()).success(function (res) {
-            var index = tripService.getIndexBYId(trip._id);
+            var index = tripService.getIndexById(trip._id);
             if(index != -1) {
                 tripService.all.splice(index, 1);
             }
@@ -41,7 +41,7 @@ app.factory('tripService', ['$http', 'authService', function ($http, authService
 
     tripService.edit = function (trip) {
         return $http.put(remote_uri(trip._id), trip, authService.getHeader()).success(function (data){
-            var index = tripService.getIndexBYId(trip._id);
+            var index = tripService.getIndexById(trip._id);
             if(index != -1) {
                 tripService.all.splice(index, 1, data);
             }
