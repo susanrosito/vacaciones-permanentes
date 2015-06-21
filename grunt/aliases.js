0afc5cc5cfea246519c219c61aa167bb1c42ebaa
Alan Rodas Bonjour <alanrodas@gmail.com>
2015-06-20 21:50:44 -0300

    Fixes #69

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,5 +1,6 @@
 module.exports = function (grunt, options) {
     return {
+        install: ['npm-install', 'bower-install-simple'],
         lint: {
             description: 'Lint and check style of the JS files',
             tasks: ['jshint', 'jscs']
@@ -8,7 +9,7 @@ module.exports = function (grunt, options) {
             description: 'Run the server at port 3000',
             tasks: ['compile', 'cwd:target', 'express', 'cwd:current', 'watch']
         },
-        test: ['lint', 'compile', 'copy:test', 'cwd:test'],
+        test: ['install', 'lint', 'compile', 'copy:test', 'cwd:test'],
         default: ['env:develop', 'run']
     };
 };
